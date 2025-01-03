cdb0bfa299f7820cf5172c10dec7008b40dcc127
Nasor2 <penaortegasamuel@gmail.com>
2025-01-03 16:46:17 -0500

    Clerk Setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,17 @@
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
+import Image from "next/image";
 
-        <main>
-            {children}
-        </main>
-    );
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image src="/images/bg-img.png" alt="background" fill className="size-full" />
+      </div>
+
+      {children}
+    </main>
+  );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,13 @@
+//https://concise-monkey-77.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://concise-monkey-77.clerk.accounts.dev" ,
+            applicationID: "convex",
+
+        }
+    ]
+}
+
+export default authConfig;
